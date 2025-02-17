6870d3abb442cd89f7d3bebe023355366d5c3265
Ganesh Jangir <ganeshnj@outlook.com>
2025-02-18 00:55:21 +0100

    Using Tablegen for Passes

--- lib/Transform/Arith/MulToAdd.cpp
+++ lib/Transform/Arith/MulToAdd.cpp
@@ -3,9 +3,11 @@
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "mlir/include/mlir/Pass/Pass.h"
-
 namespace mlir {
 namespace tutorial {
+
+#define GEN_PASS_DEF_MULTOADD
+#include "lib/Transform/Arith/Passes.h.inc"
 
 using arith::AddIOp;
 using arith::ConstantOp;
@@ -13,8 +15,7 @@ using arith::MulIOp;
 
 // Replace y = C*x with y = C/2*x + C/2*x, when C is a power of 2, otherwise do
 // nothing.
-struct PowerOfTwoExpand :
-  public OpRewritePattern<MulIOp> {
+struct PowerOfTwoExpand : public OpRewritePattern<MulIOp> {
   PowerOfTwoExpand(mlir::MLIRContext *context)
       : OpRewritePattern<MulIOp>(context, /*benefit=*/2) {}
 
@@ -22,36 +23,34 @@ struct PowerOfTwoExpand :
                                 PatternRewriter &rewriter) const override {
     Value lhs = op.getOperand(0);
 
-    // canonicalization patterns ensure the constant is on the right, if there is a constant
-    // See https://mlir.llvm.org/docs/Canonicalization/#globally-applied-rules
+    // canonicalization patterns ensure the constant is on the right, if there
+    // is a constant See
+    // https://mlir.llvm.org/docs/Canonicalization/#globally-applied-rules
     Value rhs = op.getOperand(1);
     auto rhsDefiningOp = rhs.getDefiningOp<arith::ConstantIntOp>();
     if (!rhsDefiningOp) {
       return failure();
     }
-
     int64_t value = rhsDefiningOp.value();
     bool is_power_of_two = (value & (value - 1)) == 0;
-
     if (!is_power_of_two) {
       return failure();
     }
 
     ConstantOp newConstant = rewriter.create<ConstantOp>(
-        rhsDefiningOp.getLoc(), rewriter.getIntegerAttr(rhs.getType(), value / 2));
+        rhsDefiningOp.getLoc(),
+        rewriter.getIntegerAttr(rhs.getType(), value / 2));
     MulIOp newMul = rewriter.create<MulIOp>(op.getLoc(), lhs, newConstant);
     AddIOp newAdd = rewriter.create<AddIOp>(op.getLoc(), newMul, newMul);
 
     rewriter.replaceOp(op, {newAdd});
     rewriter.eraseOp(rhsDefiningOp);
-
     return success();
   }
 };
 
 // Replace y = 9*x with y = 8*x + x
-struct PeelFromMul :
-  public OpRewritePattern<MulIOp> {
+struct PeelFromMul : public OpRewritePattern<MulIOp> {
   PeelFromMul(mlir::MLIRContext *context)
       : OpRewritePattern<MulIOp>(context, /*benefit=*/1) {}
 
@@ -63,31 +62,33 @@ struct PeelFromMul :
     if (!rhsDefiningOp) {
       return failure();
     }
-
     int64_t value = rhsDefiningOp.value();
-
     // We are guaranteed `value` is not a power of two, because the greedy
     // rewrite engine ensures the PowerOfTwoExpand pattern is run first, since
     // it has higher benefit.
 
     ConstantOp newConstant = rewriter.create<ConstantOp>(
-        rhsDefiningOp.getLoc(), rewriter.getIntegerAttr(rhs.getType(), value - 1));
+        rhsDefiningOp.getLoc(),
+        rewriter.getIntegerAttr(rhs.getType(), value - 1));
     MulIOp newMul = rewriter.create<MulIOp>(op.getLoc(), lhs, newConstant);
     AddIOp newAdd = rewriter.create<AddIOp>(op.getLoc(), newMul, lhs);
 
     rewriter.replaceOp(op, {newAdd});
     rewriter.eraseOp(rhsDefiningOp);
-
     return success();
   }
 };
 
-void MulToAddPass::runOnOperation() {
-  mlir::RewritePatternSet patterns(&getContext());
-  patterns.add<PowerOfTwoExpand>(&getContext());
-  patterns.add<PeelFromMul>(&getContext());
-  (void)applyPatternsAndFoldGreedily(getOperation(), std::move(patterns));
-}
+struct MulToAdd : impl::MulToAddBase<MulToAdd> {
+  using MulToAddBase::MulToAddBase;
+
+  void runOnOperation() {
+    mlir::RewritePatternSet patterns(&getContext());
+    patterns.add<PowerOfTwoExpand>(&getContext());
+    patterns.add<PeelFromMul>(&getContext());
+    (void)applyPatternsAndFoldGreedily(getOperation(), std::move(patterns));
+  }
+};
 
 } // namespace tutorial
 } // namespace mlir
